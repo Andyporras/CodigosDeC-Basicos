2047e4e07ccc0ef39c4d9575d3ee7d8254945c93
Andy Porras <andyporras6@gmail.com>
2021-07-29 07:58:32 -0600

    hola mundo :D
    primer código de C++
                   "Hola Mundo :D"

---
+++ primer programa.cpp
@@ -0,0 +1,14 @@
+//Primer programa c++
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	//Primer metodo para espacio(Enter)
+	cout<<"Hola mundo :D"<<endl;
+	//Segundo metodo para espacio(Enter)
+	//cout<<"Hola mundo :D"<<endl;
+	return 0;
+}
